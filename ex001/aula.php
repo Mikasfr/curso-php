df9ace2c920a7d3c2e6efab589d710c94c9aa68a
michael <michaelreques@gmail.com>
2024-12-17 00:21:11 -0300

    Tipos primitivos

---
+++ ex001/aula.php
@@ -0,0 +1,22 @@
+tipos primitivos
+
+o tipo é definido de forma dinamica, com o andamento do projeto as variaveis podem variar seu tipo
+
+existem três classes de tipos
+
+escalares - compostos - especiais
+
+
+tipos primitivos escalares:
+
+<?php
+
+    $sobrenome = "Silva"; // String - sequencia de letras, números e símbolos, sempre representados entre aspas 
+
+    $idade = 34; // int ou interger - um valor numerico inteiro, aquele que vem sem a parte decimal
+
+    $peso = 85.8; // float, double - Um valor numerico Real, que vem com a parte decimal depois do ponto flutuante 
+
+    $casado = true; // bool ou boolean - Um valor logico ou booleano, que aceita apenas valores verdadeiro ou falso (true or false)
+
+?>
--- ex002/index.php
+++ ex002/index.php
@@ -12,7 +12,28 @@
     <?php 
         date_default_timezone_set("America/Sao_Paulo"); //GMT-3
         echo "Hoje é dia: " . date("d/M/Y");
-        echo " e a hora atual é: " . date("G:i:s")
+        echo " e a hora atual é: " . date("G:i:s");
+
+        // variáveis e constantes em PHP
+
+        $nome = "Michael";
+        $sobrenome = "Reques";
+        echo " Muito prazer, $nome $sobrenome";
+
+        const country = "Brasil";
+        echo " Você mora no " . country;
+
+        // regras para identificadores de variáveis e constantes 
+        // 1 - sempre começa com $
+        // 2 - O segundo caractere poder ser letra ou símbolo _
+        // 3 - Aceita caracteres [a-z],[A-Z], [0-9] e [_]
+        // 4 - Aceita caracteres da tabela ASCII a partir de 128
+        // 5 - Aceita caracteres acentuados 
+        // 6 - A linguagem é case sensitive em relação aos nomes 
+        // 7 - Nomes especiais como $this não podem ser usados 
+
+        // exemplos 
+
     ?>
 </body>
 
